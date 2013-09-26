842f9b40c53304f18050a0e9db35c90ad3bda660
Ben <benedict@phpdevel.net>
2013-09-26 13:25:52 +0200

    Adding catalog keys

--- src/App/Console/MerchantCommand.php
+++ src/App/Console/MerchantCommand.php
@@ -14,19 +14,37 @@ class MerchantCommand extends Command
     {
         $this
             ->setName('popshops:merchants')
-            ->setDescription('List merchants');
+            ->setDescription('List merchants')
+            ->addArgument('catalog', InputArgument::OPTIONAL, 'Specify the catalog name from the configured catalog in config/global.yml or config/local.yml if exists (popshops.catalog_keys) ');
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $app = $this->getHelperSet()->get('container')->getContainer();
         $popshops = $app['popshops.client'];
-        $catalogKey = $app['popshops.default_catalog_key'];
+        $catalogs = $app['popshops.catalog_keys'];
+        $catalog = $input->getArgument('catalog');
 
-        $merchants = $popshops->getMerchants($catalogKey);
+        if (!isset($catalogs[$catalog])) {
+            $dialog = $this->getHelperSet()->get('dialog');
+            $catalog = $dialog->askAndValidate(
+                $output,
+                'Please specify the catalog (' . implode(', ', array_keys($catalogs)) . '): ',
+                function ($answer) use ($catalogs) {
+                    if (!isset($catalogs[$answer])) {
+                        throw new \RunTimeException('Invalid catalog name');
+                    }
+
+                    return $answer;
+                },
+                3
+            );
+        }
+
+        $merchants = $popshops->getMerchants($catalogs[$catalog]);
         if (count($merchants) > 0) {
             $table = $this->getHelperSet()->get('table');
-            $table->setHeaders(array('Id', 'Merchant'));
+            $table->setHeaders(['Id', 'Merchant']);
 
             foreach ($merchants as $merchant) {
                 $table->addRow(array(
