662ede193856b9a29ccf362a7f9eb152e21e8280
Ben <benedict@phpdevel.net>
2013-09-26 20:01:42 +0200

    ProductResultSet

---
+++ src/App/Popshops/ProductResultSet.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Popshops;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class ProductResultSet
+{
+    protected $keywords;
+    protected $limit;
+    protected $totalCount = 0;
+    protected $products;
+    protected $priceRanges;
+    protected $merchantTypes;
+    protected $brands;
+    protected $networks;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+        $this->priceRanges = new ArrayCollection();
+        $this->merchantTypes = new ArrayCollection();
+        $this->brands = new ArrayCollection();
+        $this->networks = new ArrayCollection();
+    }
+
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    public function getPriceRanges()
+    {
+        return $this->priceRanges;
+    }
+
+    public function getMerchantTypes()
+    {
+        return $this->merchantTypes;
+    }
+
+    public function getBrands()
+    {
+        return $this->brands;
+    }
+
+    public function getNetworks()
+    {
+        return $this->networks;
+    }
+}
