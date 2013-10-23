df0b8a0adf4a1c94a0885bb1af537af5ee361d59
Ben <benedict@phpdevel.net>
2013-10-23 17:23:29 +0200

    Call extrabux stats

--- beesavy/system/application/controllers/migrate.php
+++ beesavy/system/application/controllers/migrate.php
@@ -8,10 +8,25 @@ class Migrate extends Controller
     public function __construct()
     {
         parent::Controller();
+        $this->load->model('user');
+        $this->load->model('admin');
     }
 
     public function user_download()
     {
         echo 'Starting user download', PHP_EOL;
+
+        $users = $this->db
+            ->order_by('id', 'asc')
+            ->get('user')
+            ->result_array();
+
+        foreach ($users as $user) {
+            echo sprintf('Id: %s Email: %s', $user['id'], $user['email']), PHP_EOL;
+            $summary = $this->cache->library('beesavy', 'getUserStats', [$user['id']], 3600);
+            $referrals = $this->cache->library('extrabux', 'getUserReferrals', [$user['id']], 3600);
+            $report = $this->cache->library('extrabux', 'getUserReport', [$user['id']], 3600);
+            $stats = $this->cache->library('extrabux', 'getUserStats', [$user['id']], 3600);
+        }
     }
 }
