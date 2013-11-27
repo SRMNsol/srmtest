c783ed60bf611daa566948589fc0c03c495357ce
Ben <benedict@phpdevel.net>
2013-11-27 13:00:33 +0100

    Payable repository

--- src/App/Entity/Payable.php
+++ src/App/Entity/Payable.php
@@ -3,7 +3,7 @@
 namespace App\Entity;
 
 /**
- * @Entity
+ * @Entity(repositoryClass="App\Repository\PayableRepository")
  * @HasLifecycleCallbacks
  * @InheritanceType("SINGLE_TABLE")
  * @DiscriminatorColumn(name="payableType")
---
+++ src/App/Repository/PayableRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Orm\NoResultException;
+use App\Entity\User;
+use App\Entity\Payable;
+
+class PayableRepository extends EntityRepository
+{
+    public function calculateUserSummary(User $user)
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->select([
+                'SUM(p.amount) AS amount',
+                'SUM(p.pending) AS pending',
+                'SUM(p.available) AS available',
+                'SUM(p.processing) AS processing',
+                'SUM(p.paid) AS paid'
+            ])
+            ->where('p.user = :user')
+            ->groupBy('p.user')
+            ->setParameter('user', $user)
+        ;
+
+        try {
+            return $qb->getQuery()->getSingleResult();
+        } catch (NoResultException $e) {
+            return null;
+        }
+    }
+}
