dadf95803b5b5fd6c0f711fba3b3fb1087c5cd31
Ben <benedict@phpdevel.net>
2014-01-09 23:22:34 +0100

    Fix transaction download

--- config/popshops-cli-config.php
+++ config/popshops-cli-config.php
@@ -1,3 +1,9 @@
 <?php
 
-return require __DIR__ . '/../src/app.php';
+use Popshops\Silex\PopshopsExtraServiceProvider;
+
+$app = require __DIR__ . '/../src/app.php';
+
+$app->register(new PopshopsExtraServiceProvider());
+
+return $app;
