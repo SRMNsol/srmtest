c2c45ccc789e4468a07c0fd0573340e8f99ef83a
Ben <benedict@phpdevel.net>
2014-02-18 12:52:42 +0100

    Update transaction status

--- tests/App/Entity/TransactionTest.php
+++ tests/App/Entity/TransactionTest.php
@@ -2,6 +2,7 @@
 
 use App\Tests\OrmTestCase;
 use App\Entity\Transaction;
+use App\Entity\Payment;
 
 class TransactionTest extends OrmTestCase
 {
@@ -17,5 +18,38 @@ class TransactionTest extends OrmTestCase
         $this->em->flush();
 
         $this->assertGreaterThan(0, $transaction->getId());
+    }
+
+    public function testPaymentUpdateTransactionStatus()
+    {
+        $transaction = new Transaction();
+        $transaction->setRegisteredAt(new DateTime());
+        $transaction->setOrderNumber('T123');
+        $transaction->setCommission(10.00);
+        $this->em->persist($transaction);
+        $this->em->flush();
+
+        $this->assertEquals(Transaction::STATUS_REGISTERED, $transaction->getStatus());
+
+        $payment = new Payment();
+        $payment->setPaymentAt(new \DateTime());
+        $payment->setPaymentNumber('P123');
+        $payment->setTotal(5.00);
+        $transaction->addPayment($payment);
+        $this->em->persist($payment);
+        $this->em->flush();
+
+        $this->assertEquals(Transaction::STATUS_AVAILABLE, $transaction->getStatus());
+
+        $payment = new Payment();
+        $payment->setPaymentAt(new \DateTime());
+        $payment->setPaymentNumber('P234');
+        $payment->setTotal(3.00);
+        $payment->setAdjustment(2.00);
+        $transaction->addPayment($payment);
+        $this->em->persist($payment);
+        $this->em->flush();
+
+        $this->assertEquals(Transaction::STATUS_PROCESSED, $transaction->getStatus());
     }
 }
