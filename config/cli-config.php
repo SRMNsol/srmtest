9fb571b1c710ddf5f7405387d1e7fda22c06229d
Ben <benedict@phpdevel.net>
2014-06-20 14:02:54 +0200

    Doctrine migrations setup

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,9 +1,25 @@
 <?php
+
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Symfony\Component\Console\Helper\DialogHelper;
+use Doctrine\DBAL\Migrations\Tools\Console\Command as Migrations;
 
 $app = require __DIR__ . '/../src/app.php';
 
 // initialize db to register enum type
 $db = $app['db'];
 
-return ConsoleRunner::createHelperSet($app['orm.em']);
+$helperSet = ConsoleRunner::createHelperSet($app['orm.em']);
+
+// add dialog
+$helperSet->set(new DialogHelper(), 'dialog');
+
+// add migrations commands
+$commands[] = new Migrations\DiffCommand();
+$commands[] = new Migrations\ExecuteCommand();
+$commands[] = new Migrations\GenerateCommand();
+$commands[] = new Migrations\MigrateCommand();
+$commands[] = new Migrations\StatusCommand();
+$commands[] = new Migrations\VersionCommand();
+
+return $helperSet;
