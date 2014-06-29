96aedf64a0816780b19cf0e4dc49adc4ff3616ab
Ben <benedict@phpdevel.net>
2014-06-29 19:59:35 +0200

    Update links to s3 for common elements

--- legacy/system/application/helpers/s3_helper.php
+++ legacy/system/application/helpers/s3_helper.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Path from S3 bucket
+ *
+ * vim
+ * %s/\v(src|href) *\= *["']\/?([^http|^<\?php][^ ]{-}\..{-})["']/\1="<?php echo s3path("\/\2") ?>"/g
+ */
 function s3path($file) {
-    return 'http://static0.beesavy.com/' . $file;
+    return 'http://static0.beesavy.com' . $file;
 }
--- legacy/system/application/views/blocks/nav_bar.php
+++ legacy/system/application/views/blocks/nav_bar.php
@@ -1,18 +1,18 @@
 <div id="nav">
-              <div id="nav-left-curve"><img src="/images/nav-left-curve.gif" width="8" height="42" alt="** PLEASE DESCRIBE THIS IMAGE **" /></div>
+              <div id="nav-left-curve"><img src="<?php echo s3path("/images/nav-left-curve.gif") ?>" width="8" height="42" alt="** PLEASE DESCRIBE THIS IMAGE **" /></div>
               <div class="nav-bg" style="border:0px solid #000;width:914px;height:42px;float:left;margin-top:0px;">
-              <div id="nav-ShopByCatagory-bt"><img src="/images/nav_catagoryBt.jpg" width="140" height="42" alt="** PLEASE DESCRIBE THIS IMAGE **" onclick="$('#catDropdown1').slideToggle();"/></div>
-              <div id="nav-SearchLabel"><img src="/images/nav_searchLabel.jpg" width="114" height="42" alt="** PLEASE DESCRIBE THIS IMAGE **"/></div>
+              <div id="nav-ShopByCatagory-bt"><img src="<?php echo s3path("/images/nav_catagoryBt.jpg") ?>" width="140" height="42" alt="** PLEASE DESCRIBE THIS IMAGE **" onclick="$('#catDropdown1').slideToggle();"/></div>
+              <div id="nav-SearchLabel"><img src="<?php echo s3path("/images/nav_searchLabel.jpg") ?>" width="114" height="42" alt="** PLEASE DESCRIBE THIS IMAGE **"/></div>
               <form id="nav-search-form" action="/search" method="get" accept-charset="utf-8">
               <div id="nav-search">
               <input id="nav-search-input" class="input-box input-focus ac_input" size="10px" name="q" value="Enter a product or store..." type="text"/>
-              <input id="nav-search-form-image" src="/images/nav-searchBt.jpg" type="image"/>
+              <input id="nav-search-form-image" src="<?php echo s3path("/images/nav-searchBt.jpg") ?>" type="image"/>
               </div>
               </form>
               <div class="ShopByStore"><a href="/stores/search" class="nav-ShopByStore-Bt BtnSBSOrangeBg"></a></div>
               <div class="FindCoupons"><a href="/coupon/search" class="nav-FindCoupons-Bt BtnFCOrangeBg"></a></div>
               </div>
-              <div id="nav-right-curve"><img src="/images/nav-right-curve.gif" width="8" height="42" alt="** PLEASE DESCRIBE THIS IMAGE **"/></div>
+              <div id="nav-right-curve"><img src="<?php echo s3path("/images/nav-right-curve.gif") ?>" width="8" height="42" alt="** PLEASE DESCRIBE THIS IMAGE **"/></div>
       </div>
       <div>
       <div id="catDropdown1" style="float:left;width:921px;height:320px;display: none;">
