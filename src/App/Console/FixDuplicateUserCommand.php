78a43e88985d0183bbe83c8c8af4be9d4f90a705
Ben <benedict@phpdevel.net>
2014-07-15 19:52:18 +0200

    Make transactional

--- src/App/Console/FixDuplicateUserCommand.php
+++ src/App/Console/FixDuplicateUserCommand.php
@@ -22,86 +22,98 @@ class FixDuplicateUserCommand extends Command
         $app = $this->getSilexApplication();
         $em = $app['orm.em'];
 
-        // fix duplicate emails
-        $qb = $em->createQueryBuilder();
-        $qb->select(['u.email', 'COUNT(u)']);
-        $qb->from('App\Entity\User', 'u');
-        $qb->groupBy('u.email');
-        $qb->having($qb->expr()->gt('COUNT(u)', 1));
+        $em->getConnection()->beginTransaction();
 
-        $results = $qb->getQuery()->execute();
+        try {
 
-        foreach ($results as $result) {
+            // fix duplicate emails
+            $qb = $em->createQueryBuilder();
+            $qb->select(['u.email', 'COUNT(u)']);
+            $qb->from('App\Entity\User', 'u');
+            $qb->groupBy('u.email');
+            $qb->having($qb->expr()->gt('COUNT(u)', 1));
 
-            $users = $em->getRepository('App\Entity\User')->findBy(['email' => $result['email']], ['id' => 'ASC']);
-            $originalUser = array_shift($users);
+            $results = $qb->getQuery()->execute();
 
-            $output->writeln(sprintf('Email: %s (%d)', $originalUser->getEmail(), $result[1]));
+            foreach ($results as $result) {
 
-            foreach ($users as $user) {
+                $users = $em->getRepository('App\Entity\User')->findBy(['email' => $result['email']], ['id' => 'ASC']);
+                $originalUser = array_shift($users);
 
-                // move all referred users to original user
-                foreach ($user->getReferredUsers() as $referredUser) {
-                    $referredUser->setReferredBy($originalUser);
-                }
+                $output->writeln(sprintf('[%s] (%d)', $originalUser->getEmail(), $result[1]));
 
-                // process payables
-                foreach ($user->getPayables() as $payable) {
-                    if ($payable instanceof Referral) {
-                        // Monthly referrals are summed to original user's
-                        $matchingReferral = $originalUser->getPayables()->filter(function ($p) use ($payable) {
-                            return ($p instanceof Referral) && ($p->getMonth() === $payable->getMonth());
-                        })->current();
+                foreach ($users as $user) {
 
-                        if (isset($matchingReferral)) {
-                            $matchingReferral->setAmount($matchingReferral->getAmount() + $payable->getAmount());
-                            $matchingReferral->setDirect($matchingReferral->getDirect() + $payable->getDirect());
-                            $matchingReferral->setIndirect($matchingReferral->getIndirect() + $payable->getIndirect());
-                            $matchingReferral->setPending($matchingReferral->getPending() + $payable->getPending());
-                            $matchingReferral->setAvailable($matchingReferral->getAvailable() + $payable->getAvailable());
+                    // move all referred users to original user
+                    foreach ($user->getReferredUsers() as $referredUser) {
+                        $output->writeln(sprintf('< %s', $referredUser->getEmail()));
+                        $referredUser->setReferredBy($originalUser);
+                    }
 
-                            $output->writeln(sprintf('+ Referral %s $%.2f', $payable->getMonth(), $payable->getAmount()));
-                            $em->remove($payable);
+                    // process payables
+                    foreach ($user->getPayables() as $payable) {
+                        if ($payable instanceof Referral) {
+                            // Monthly referrals are summed to original user's
+                            $matchingReferral = $originalUser->getPayables()->filter(function ($p) use ($payable) {
+                                return ($p instanceof Referral) && ($p->getMonth() === $payable->getMonth());
+                            })->current();
+
+                            if (isset($matchingReferral)) {
+                                $matchingReferral->setAmount($matchingReferral->getAmount() + $payable->getAmount());
+                                $matchingReferral->setDirect($matchingReferral->getDirect() + $payable->getDirect());
+                                $matchingReferral->setIndirect($matchingReferral->getIndirect() + $payable->getIndirect());
+                                $matchingReferral->setPending($matchingReferral->getPending() + $payable->getPending());
+                                $matchingReferral->setAvailable($matchingReferral->getAvailable() + $payable->getAvailable());
+
+                                $output->writeln(sprintf('+ Referral %s $%.2f', $payable->getMonth(), $payable->getAmount()));
+                                $em->remove($payable);
+                            } else {
+                                $payable->setUser($originalUser);
+                                $output->writeln(sprintf('+ %s $%.2f', $payable->getConcept(), $payable->getAmount()));
+                            }
+
                         } else {
+                            // Cashback and others are assigned to original user
                             $payable->setUser($originalUser);
                             $output->writeln(sprintf('+ %s $%.2f', $payable->getConcept(), $payable->getAmount()));
                         }
 
-                    } else {
-                        // Cashback and others are assigned to original user
-                        $payable->setUser($originalUser);
-                        $output->writeln(sprintf('+ %s $%.2f', $payable->getConcept(), $payable->getAmount()));
+                        $em->remove($user);
                     }
-
-                    $em->remove($user);
                 }
             }
-        }
 
-        $em->flush();
+            $em->flush();
 
-        // fix duplicate alias
-        $qb = $em->createQueryBuilder();
-        $qb->select(['u.alias', 'COUNT(u)']);
-        $qb->from('App\Entity\User', 'u');
-        $qb->groupBy('u.alias');
-        $qb->having($qb->expr()->gt('COUNT(u)', 1));
+            // fix duplicate alias
+            $qb = $em->createQueryBuilder();
+            $qb->select(['u.alias', 'COUNT(u)']);
+            $qb->from('App\Entity\User', 'u');
+            $qb->groupBy('u.alias');
+            $qb->having($qb->expr()->gt('COUNT(u)', 1));
 
-        $results = $qb->getQuery()->execute();
+            $results = $qb->getQuery()->execute();
 
-        foreach ($results as $result) {
+            foreach ($results as $result) {
 
-            $users = $em->getRepository('App\Entity\User')->findBy(['alias' => $result['alias']], ['id' => 'ASC']);
-            $originalUser = array_shift($users);
+                $users = $em->getRepository('App\Entity\User')->findBy(['alias' => $result['alias']], ['id' => 'ASC']);
+                $originalUser = array_shift($users);
 
-            $output->writeln(sprintf('Alias: %s %s (%d)', $originalUser->getEmail(), $result['alias'], $result[1]));
+                $output->writeln(sprintf('Alias: %s %s (%d)', $originalUser->getEmail(), $result['alias'], $result[1]));
 
-            foreach ($users as $user) {
-                $user->setAlias(sprintf('U%d', $user->getId()));
-                $output->writeln(sprintf('+ %s %s', $user->getEmail(), $user->getAlias()));
+                foreach ($users as $user) {
+                    $user->setAlias(sprintf('U%d', $user->getId()));
+                    $output->writeln(sprintf('+ %s %s', $user->getEmail(), $user->getAlias()));
+                }
             }
+
+            $em->flush();
+            $em->getConnection()->commit();
+        } catch (\Exception $e) {
+            $em->getConnection()->rollback();
+            $em->close();
+            throw $e;
         }
 
-        $em->flush();
     }
 }
