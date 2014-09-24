e4c5868357d368d05a8aba73f01f25b7333854cc
Ben <benedict@phpdevel.net>
2014-09-24 20:53:35 +0200

    Use rounding on commission

--- tests/App/Entity/TransactionTest.php
+++ tests/App/Entity/TransactionTest.php
@@ -3,6 +3,7 @@
 use App\Tests\OrmTestCase;
 use App\Entity\Transaction;
 use App\Entity\Payment;
+use App\Entity\Rate;
 
 class TransactionTest extends OrmTestCase
 {
@@ -18,5 +19,44 @@ class TransactionTest extends OrmTestCase
         $this->em->flush();
 
         $this->assertGreaterThan(0, $transaction->getId());
+    }
+
+    /**
+     * Test calculation of commission by level.
+     */
+    public function testCalculateCommission()
+    {
+        $transaction = new Transaction();
+        $transaction->setCommission(100);
+
+        $rate = new Rate();
+        $rate->setLevel0(0.5);
+        $rate->setLevel1(0.05);
+        $transaction->setRate($rate);
+
+        $this->assertEquals(50, $transaction->getCommissionByLevel(0), 0.01);
+        $this->assertEquals(5, $transaction->getCommissionByLevel(1), 0.01);
+    }
+
+    /**
+     * Test rounding when commission is small
+     * Issue #78
+     */
+    public function testRoundingCommission()
+    {
+        $rate = new Rate();
+        $rate->setLevel0(0.5);
+        $rate->setLevel1(0.05);
+
+        $transaction = new Transaction();
+        $transaction->setRate($rate);
+
+        $transaction->setCommission(0.10);
+        $this->assertEquals(0.05, $transaction->getCommissionByLevel(0), 0.01);
+        $this->assertEquals(0, $transaction->getCommissionByLevel(1), 0.01);
+
+        $transaction->setCommission(7.75);
+        $this->assertEquals(3.87, $transaction->getCommissionByLevel(0), 0.01);
+        $this->assertEquals(0.38, $transaction->getCommissionByLevel(1), 0.01);
     }
 }
