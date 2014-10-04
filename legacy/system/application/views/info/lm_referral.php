cc3b732a71ebc30dd094669ee7ccb8ef587907e9
Ben <benedict@phpdevel.net>
2014-10-04 18:12:42 +0200

    Learn more referral page ads

--- legacy/system/application/views/info/lm_referral.php
+++ legacy/system/application/views/info/lm_referral.php
@@ -4,96 +4,77 @@
 <body>
 <div id="container">
 <!-- Header -->
-      {banner}
+{banner}
 <!-- /Header -->
 
-
 <!-- Navigation bar -->
-      {nav_bar}
+{nav_bar}
 <!-- /Navigation bar -->
 
+<?php echo googletag_ad("BS_help_728x90_1") ?>
 
 <!-- content -->
 <div class="BGLeftCol">
-		<!-- page Title -->
+    <!-- page Title -->
+    <div id="pageTitle">
+        <div id="pageTitleLeft"></div>
+        <h1>Help</h1>
+        <div id="pageTitleRight"></div>
+    </div>
+    <!-- /page Title -->
 
-		<div id="pageTitle">
-		<div id="pageTitleLeft"></div>
-		<h1>Help</h1>
-		<div id="pageTitleRight"></div>
-	</div>
-   		<!-- /page Title -->
+    <!-- Left category -->
+    {side_nav}
+    <!-- /Left category -->
 
-
-   		<!-- Left category -->
-
-{side_nav}
-       <!-- /Left category -->
-
-
-       <!-- Right side -->
-       <div id="results" class="help" style="border:0px solid #000;" >
-			<div class="title">Learn More - Referral Cash Back</div>
-            <div style="float:left;width:100%;"><hr color="#e96d08" style="margin-left:10px;"></div>
-
-<p><STRONG>THIS IS WHERE IT GETS REALLY EXCITING!</STRONG> &nbsp;Other shopping sites pay you a small, one-time bonus for referring your friends (usually around <strong>$5</strong>). &nbsp;We believe your referrals are much more valuable than that . . . and we're willing to put our money where our mouth is.
-
-<p><strong>How would you like to earn real money forever, just by helping others save with BeeSavy?</strong> &nbsp;We pay you an amazing <strong>10% commission</strong> on all of the cash back earned by people you refer to BeeSavy for life! &nbsp;In fact, we pay you <strong>10% commission</strong> on <STRONG>ALL</STRONG> of the people that they refer, down through <strong>seven (7) </strong>levels deep!
-</p>
-
-<p>That's it! There is no small print, no usage requirements, and no cost! Just refer your friends and you will earn commission every time they shop.</p>
-
-<p>Got a website? BeeSavy gives you an entirely new way to monetize your site. Use our banners to refer your visitors and earn commission every time they shop online. Contact us for cross-promotion opportunities.</p>
-
-<p><iframe width="425" height="349" src="http://www.youtube.com/embed/OGo_bO1aOK8?hl=en&fs=1" frameborder="0" allowfullscreen></iframe></p>
-
-	</div>
+    <!-- Right side -->
+    <div id="results" class="help" style="border:0px solid #000;" >
+        <div class="title">Learn More - Referral Cash Back</div>
+        <div style="float:left;width:100%;"><hr color="#e96d08" style="margin-left:10px;"></div>
+        <p><STRONG>THIS IS WHERE IT GETS REALLY EXCITING!</STRONG> &nbsp;Other shopping sites pay you a small, one-time bonus for referring your friends (usually around <strong>$5</strong>). &nbsp;We believe your referrals are much more valuable than that . . . and we're willing to put our money where our mouth is.
+        <p><strong>How would you like to earn real money forever, just by helping others save with BeeSavy?</strong> &nbsp;We pay you an amazing <strong>10% commission</strong> on all of the cash back earned by people you refer to BeeSavy for life! &nbsp;In fact, we pay you <strong>10% commission</strong> on <STRONG>ALL</STRONG> of the people that they refer, down through <strong>seven (7) </strong>levels deep!</p>
+        <p>That's it! There is no small print, no usage requirements, and no cost! Just refer your friends and you will earn commission every time they shop.</p>
+        <p>Got a website? BeeSavy gives you an entirely new way to monetize your site. Use our banners to refer your visitors and earn commission every time they shop online. Contact us for cross-promotion opportunities.</p>
+        <p><iframe width="425" height="349" src="http://www.youtube.com/embed/OGo_bO1aOK8?hl=en&fs=1" frameborder="0" allowfullscreen></iframe></p>
+    </div>
+    <div style="clear: both;"></div>
+</div>
 <div style="clear: both;"></div>
-					</div>
-		<div style="clear: both;"></div>
 
-       <!-- Right side -->
+<!-- Right side -->
 
 <script>
 $(document).ready(function() {
     $("div.productResult").mouseover(function () {
         var element = $(this);
- 		element.find('.BtnComparePrice').addClass('BtnOrangeRBg').removeClass('BtnOrangeBg');
+        element.find('.BtnComparePrice').addClass('BtnOrangeRBg').removeClass('BtnOrangeBg');
     }).mouseout(function () {
-    	var element = $(this);
-		element.find('.BtnComparePrice').addClass('BtnOrangeBg').removeClass('BtnOrangeRBg');
+        var element = $(this);
+        element.find('.BtnComparePrice').addClass('BtnOrangeBg').removeClass('BtnOrangeRBg');
     });
-
-
     $("div.ShopByStore").mouseover(function () {
         var element = $(this);
-		element.find('.nav-ShopByStore-Bt').addClass('BtnSBSOrangeRBg').removeClass('BtnSBSOrangeBg');
+        element.find('.nav-ShopByStore-Bt').addClass('BtnSBSOrangeRBg').removeClass('BtnSBSOrangeBg');
     }).mouseout(function () {
-    	var element = $(this);
-		element.find('.nav-ShopByStore-Bt').addClass('BtnSBSOrangeBg').removeClass('BtnSBSOrangeRBg');
-    });
-
-	    $("div.FindCoupons").mouseover(function () {
         var element = $(this);
-		element.find('.nav-FindCoupons-Bt').addClass('BtnFCOrangeRBg').removeClass('BtnFCOrangeBg');
-    }).mouseout(function () {
-    	var element = $(this);
-		element.find('.nav-FindCoupons-Bt').addClass('BtnFCOrangeBg').removeClass('BtnFCOrangeRBg');
+        element.find('.nav-ShopByStore-Bt').addClass('BtnSBSOrangeBg').removeClass('BtnSBSOrangeRBg');
     });
-
-	});
+    $("div.FindCoupons").mouseover(function () {
+        var element = $(this);
+        element.find('.nav-FindCoupons-Bt').addClass('BtnFCOrangeRBg').removeClass('BtnFCOrangeBg');
+    }).mouseout(function () {
+        var element = $(this);
+        element.find('.nav-FindCoupons-Bt').addClass('BtnFCOrangeBg').removeClass('BtnFCOrangeRBg');
+    });
+});
 </script>
-
 
 <!-- /content -->
 
-
 <!-- footer -->
-    {footer}
-  </div>
+{footer}
+<?php echo googletag_ad("BS_help_728x90_2") ?>
 <!-- /footer -->
 
-
-
-  </body>
-  </html>
+</body>
+</html>
