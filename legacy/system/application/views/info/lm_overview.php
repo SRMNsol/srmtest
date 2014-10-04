922beb2142e5d31193b6533bc561b90808116e00
Ben <benedict@phpdevel.net>
2014-10-04 18:10:26 +0200

    Learn more overview page ads

--- legacy/system/application/views/info/lm_overview.php
+++ legacy/system/application/views/info/lm_overview.php
@@ -3,104 +3,78 @@
 {header}
 <body>
 <div id="container">
-<!-- Header -->      
-      {banner}
+<!-- Header -->
+{banner}
 <!-- /Header -->
 
 <!-- Navigation bar -->
-      {nav_bar}
+{nav_bar}
 <!-- /Navigation bar -->
 
+<?php echo googletag_ad("BS_help_728x90_1") ?>
 
 <!-- content -->
 <div class="BGLeftCol">
-		<!-- page Title -->
+    <!-- page Title -->
+    <div id="pageTitle">
+        <div id="pageTitleLeft"></div>
+        <h1>Help</h1>
+        <div id="pageTitleRight"></div>
+    </div>
+    <!-- /page Title -->
 
-		<div id="pageTitle">
-		<div id="pageTitleLeft"></div>
-		<h1>Help</h1>
-		<div id="pageTitleRight"></div>
-	</div>
-   		<!-- /page Title -->
-    
-    	
-   		<!-- Left category -->
+    <!-- Left category -->
+    {side_nav}
+    <!-- /Left category -->
 
-{side_nav}
-        
-       <!-- /Left category -->
-       
-       
-       <!-- Right side -->
-
-            
-       <div id="results" class="help" >
-       			<div class="title">Learn More - Overview</div>
-            <div style="float:left;width:100%;"><hr color="#e96d08" style="margin-left:10px;"></div>
-
-<p><strong><strong>Have you always wanted a personal shopper, but didn't have the money for your own assistant?</strong></strong> &nbsp;BeeSavy is your digital shopping assistant — that pays <strong><STRONG>YOU</STRONG></strong> for the privilege of serving you! &nbsp;BeeSavy scours the internet to ensure that you get the lowest prices on the products you want at the stores <strong><STRONG>YOU </STRONG></strong>love. &nbsp;
-</p>
-
-<p><strong>Are you a coupon clipper?</strong> &nbsp;Let BeeSavy do the work for you. &nbsp;We'll do the legwork and find any applicable coupons to lower your price even further. 
-</p>
-
-<p><strong>Want an even sweeter deal?</strong> &nbsp;We'll pay you cash back on your purchases! 
-</p>
-
-<p><strong>What could be better than getting paid to shop?</strong> &nbsp;Getting paid when other people shop! &nbsp;Tell your friends about BeeSavy, and we'll pay you commission on all of their purchases <strong>. . .</strong> <STRONG>FOREVER!</STRONG> &nbsp;We'll even provide you with the tools to make getting the buzz out easy. 
-
-
-<br><br><iframe width="425" height="349" src="http://www.youtube.com/embed/XFqy-8xjcpw?hl=en&fs=1" frameborder="0" allowfullscreen></iframe><br><br>
-
-
-
-	</div>
+    <!-- Right side -->
+    <div id="results" class="help" >
+        <div class="title">Learn More - Overview</div>
+        <div style="float:left;width:100%;"><hr color="#e96d08" style="margin-left:10px;"></div>
+        <p><strong><strong>Have you always wanted a personal shopper, but didn't have the money for your own assistant?</strong></strong> &nbsp;BeeSavy is your digital shopping assistant — that pays <strong><STRONG>YOU</STRONG></strong> for the privilege of serving you! &nbsp;BeeSavy scours the internet to ensure that you get the lowest prices on the products you want at the stores <strong><STRONG>YOU </STRONG></strong>love. &nbsp;</p>
+        <p><strong>Are you a coupon clipper?</strong> &nbsp;Let BeeSavy do the work for you. &nbsp;We'll do the legwork and find any applicable coupons to lower your price even further.</p>
+        <p><strong>Want an even sweeter deal?</strong> &nbsp;We'll pay you cash back on your purchases!</p>
+        <p><strong>What could be better than getting paid to shop?</strong> &nbsp;Getting paid when other people shop! &nbsp;Tell your friends about BeeSavy, and we'll pay you commission on all of their purchases <strong>. . .</strong> <STRONG>FOREVER!</STRONG> &nbsp;We'll even provide you with the tools to make getting the buzz out easy.
+        <br><br><iframe width="425" height="349" src="http://www.youtube.com/embed/XFqy-8xjcpw?hl=en&fs=1" frameborder="0" allowfullscreen></iframe><br><br>
+    </div>
+    <div style="clear: both;"></div>
+</div>
 <div style="clear: both;"></div>
-					</div>
-		<div style="clear: both;"></div>
 
-       <!-- Right side -->
+<!-- Right side -->
 
 <script>
 $(document).ready(function() {
     $("div.productResult").mouseover(function () {
         var element = $(this);
- 		element.find('.BtnComparePrice').addClass('BtnOrangeRBg').removeClass('BtnOrangeBg');
+        element.find('.BtnComparePrice').addClass('BtnOrangeRBg').removeClass('BtnOrangeBg');
     }).mouseout(function () {
-    	var element = $(this);
-		element.find('.BtnComparePrice').addClass('BtnOrangeBg').removeClass('BtnOrangeRBg');
+        var element = $(this);
+        element.find('.BtnComparePrice').addClass('BtnOrangeBg').removeClass('BtnOrangeRBg');
     });
-	
-		
     $("div.ShopByStore").mouseover(function () {
         var element = $(this);
-		element.find('.nav-ShopByStore-Bt').addClass('BtnSBSOrangeRBg').removeClass('BtnSBSOrangeBg');
+        element.find('.nav-ShopByStore-Bt').addClass('BtnSBSOrangeRBg').removeClass('BtnSBSOrangeBg');
     }).mouseout(function () {
-    	var element = $(this);
-		element.find('.nav-ShopByStore-Bt').addClass('BtnSBSOrangeBg').removeClass('BtnSBSOrangeRBg');
-    });
-	
-	    $("div.FindCoupons").mouseover(function () {
         var element = $(this);
-		element.find('.nav-FindCoupons-Bt').addClass('BtnFCOrangeRBg').removeClass('BtnFCOrangeBg');
-    }).mouseout(function () {
-    	var element = $(this);
-		element.find('.nav-FindCoupons-Bt').addClass('BtnFCOrangeBg').removeClass('BtnFCOrangeRBg');
+        element.find('.nav-ShopByStore-Bt').addClass('BtnSBSOrangeBg').removeClass('BtnSBSOrangeRBg');
     });
-	
-	});
+    $("div.FindCoupons").mouseover(function () {
+        var element = $(this);
+        element.find('.nav-FindCoupons-Bt').addClass('BtnFCOrangeRBg').removeClass('BtnFCOrangeBg');
+    }).mouseout(function () {
+        var element = $(this);
+        element.find('.nav-FindCoupons-Bt').addClass('BtnFCOrangeBg').removeClass('BtnFCOrangeRBg');
+    });
+});
 </script>
-
 
 <!-- /content -->
 
-<!-- footer -->  
-    {footer}
-  </div>
-<!-- /footer --> 
+<!-- footer -->
+{footer}
+<?php echo googletag_ad("BS_help_728x90_2") ?>
+<!-- /footer -->
 
-
-
-
-  </body>
-  </html>
+</body>
+</html>
