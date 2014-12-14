2a44dc2857735556ab62c7e0bf833e9da6ae42cb
Benedict <benedict@phpdevel.net>
2014-12-15 00:05:36 +0100

    Install s3cmd

--- development.sh
+++ development.sh
@@ -4,7 +4,7 @@
 HOST_IP=`route -n | grep UG | grep -Eo '[1-9][0-9]*\.[0-9]+\.[0-9]+\.[0-9]+'`
 
 # php
-apt-get install php5-xdebug
+apt-get install -y php5-xdebug
 
 cat <<CONF > /etc/php5/mods-available/dev.ini
 display_errors = 1
@@ -17,3 +17,6 @@ rm /etc/php5/cli/conf.d/9*
 (cd /etc/php5/cli/conf.d && ln -sf ../../mods-available/dev.ini 90-dev.ini)
 rm /etc/php5/apache2/conf.d/9*
 (cd /etc/php5/apache2/conf.d && ln -sf ../../mods-available/dev.ini 90-dev.ini)
+
+# s3cmd
+apt-get install -y s3cmd
